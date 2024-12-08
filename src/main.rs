9cba3f05c547d08ce7bd5b0134130a34a511392e
Myra Pearson <myra.l.pearson@gmail.com>
2024-12-07 17:37:29 -0700

    feat: lists all prime between 1-100
    - Created code that lists all prime numbers between 1 and 100.
    - Created data structure that validates every number between 1 and 100 and makes sure that it's a prime number
    - added dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use anyhow::Result;
+use CrypticRust::prelude::Primality;
+use CrypticRust::primality;
+
 fn main() {
-    println!("Hello, world!");
+    let only_primes = only_primes();
+    println!("My List:\n");
+    only_primes.into_iter().for_each(|item| println!("{:?}", item));
+}
+
+fn prime_nums() -> PrimeList {
+    PrimeList::new(vec![]).fill()
+}
+
+fn only_primes() -> Result<PrimeList> {
+    let numbers: Vec<usize> = (0..100).into_iter().collect();
+    PrimeList::new(numbers).prune()
+}
+
+#[derive(Clone, Debug, PartialOrd, PartialEq)]
+pub struct PrimeList {
+    pub list: Vec<usize>,
+}
+
+impl PrimeList {
+    pub fn new(list: Vec<usize>) -> PrimeList {
+        PrimeList { list }
+    }
+
+    fn fill(self) -> PrimeList {
+        let vector = vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 31];
+
+        PrimeList { list: vector }
+    }
+
+    fn prune(self) -> Result<PrimeList> {
+        // Return only the prime numbers within self
+      let list =  self.list.into_iter().filter(|i|i.primality()).collect();
+
+        Ok(PrimeList { list })
+    }
 }
